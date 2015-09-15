0feb74077e9509c9f4c90fb58237d5834a88da67
BeataSlowik <milvusia@gmail.com>
2015-09-15 22:00:26 +0200

    modified:   index.html 	modified:   script/script.js 	modified:   style/style.css

--- script/script.js
+++ script/script.js
@@ -1,48 +1,101 @@
 $(document).on("ready", function() {
 
+  var firstNumberValue;
+  var secondNumberValue;
+  var resultValue;
 
+  function takeValue(idOfValue) {
+    return $(idOfValue).val().trim().replace(",", ".");
+  }
 
-  function count(sign) {
-    var firstNumberValue = $("#firstNumber").val().trim().replace(",", ".");
-    var secondNumberValue = $("#secondNumber").val().trim().replace(",", ".");
+  function printElement(sign) {
     var listOfElements = $("ul");
+    var elementOfList = $("<li> <span>" + firstNumberValue + " " + sign + " " + secondNumberValue + " = " + resultValue + "</span></li>");
+    listOfElements.append(elementOfList);
+    var buttonMark = $("<span>Zaznacz</span>").addClass("buttonMark");
+    elementOfList.append(buttonMark);
+  }
 
-    if ($.isNumeric(firstNumberValue) === true && $.isNumeric(secondNumberValue) === true) {
+  function printAlert() {
+    alert("Podaj liczby");
+  }
 
+  function checkIfNumber(value) {
+    var result = false;
+    if ($.isNumeric(value)) {
+      result = true;
+    } else {
+      printAlert();
+    }
+    return result;
+  }
+
+  function parseNumbers() {
+    var result = false;
+    if (checkIfNumber(firstNumberValue) && checkIfNumber(secondNumberValue)) {
       firstNumberValue = parseFloat(firstNumberValue);
       secondNumberValue = parseFloat(secondNumberValue);
+      result = true;
+    }
+    return result;
+  }
 
-      resultValue = firstNumberValue + secondNumberValue;
+  var add = function(a, b) {
+    return a + b;
+  }
 
-      var elementOfList = $("<li>" + firstNumberValue + sign + secondNumberValue + " = " + resultValue + "</li>");
-      listOfElements.append(elementOfList);
-      var buttonMark = $("<button>Zaznacz</button>")
-      elementOfList.append(buttonMark);
+  var sub = function(a, b) {
+    return a - b;
+  }
 
-      console.log(resultValue);
+  var multiply = function(a, b) {
+    return a * b;
+  }
 
+  var divide = function(a, b) {
+    return a / b;
+  }
 
-    } else {
-      alert("Podaj liczby");
+  function count(sign, method) {
+    firstNumberValue = takeValue("#firstNumber");
+    secondNumberValue = takeValue("#secondNumber");
+
+    if (parseNumbers()) {
+      resultValue = method(firstNumberValue, secondNumberValue);
+      printElement(sign);
     }
   }
 
+  ///////
 
   $("#buttonAdd").on("click",
     function() {
-      return count("+");
+      return count("+", add);
     }
   )
 
-
-
-  // =======================================================================================================
-
-
-  var myList = $("ul");
-  myList.on("click", "button",
+  $("#buttonSubtraction").on("click",
     function() {
-      $(this).parent("li").toggleClass("mark");
+      return count("-", sub);
+    }
+  )
+
+  $("#buttonMultiplication").on("click",
+    function() {
+      return count("*", multiply);
+    }
+  )
+
+  $("#buttonDivision").on("click",
+    function() {
+      return count("/", divide);
+    }
+  )
+
+  // MARK RED
+  $("ul").on("click", ".buttonMark",
+    function() {
+      $(this).prev("span").toggleClass("mark");
     }
   )
 
